937e836ee4cb5cab26c06ece548618f9c6c4c83a
Bohnna Chhim <info@bohnna.com>
2015-02-09 20:59:21 -0500

    arraytolist.js progress

--- CH4/arraytolist.js
+++ CH4/arraytolist.js
@@ -1,12 +1,27 @@
 function arrayToList(arr) {
-	list = {}
-		for (i = 0; i <= arr.length - 1; i++) {
-			list.push(value: arr.shift(), rest: arr.slice(i))
-		}
-		return list;
+	list = {};
+	for (i = arr.length - 1; i >= 0; i--) {
+		list = {value: arr[i], rest: list};	
+	}
+	return list;
 }
 
-function listToArray() {
+function listToArray(lta) {
+	arr = [];
+	for (var node = lta; node; node = node.rest) {
+		if (node.value) {
+			arr.push(node.value);
+		}
+	}
+	return arr;
+}
+
+function prepend(x, y) {
+	newlist = {value: x, rest: y};
+	return newlist;
+}
+
+function nth(x, y) {
 
 }
 
@@ -16,5 +31,5 @@ console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
 console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-console.log(nth(arrayToList([10, 20, 30]), 1));
+// console.log(nth(arrayToList([10, 20, 30]), 1));
 // → 20
